5730747e332548fcf7ea37d45a07fcf81d304c85
aiwao <233898056+aiwao@users.noreply.github.com>
2026-03-18 17:35:39 +0900

    feat: rust environment

---
+++ rust/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A basic flake for Rust";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, fenix, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rustToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-qqF33vNuAdU5vua96VKVIwuc43j4EFeEXbjQ6+l4mO4=";
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs;[
+            pkg-config
+            openssl
+          ];
+          packages = with pkgs;[
+            rustToolchain
+            rust-analyzer
+          ];
+        };
+
+        env = {
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
